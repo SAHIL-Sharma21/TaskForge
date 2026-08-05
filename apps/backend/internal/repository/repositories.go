31ee9c2a5a5cc6c1eb5a15c39f8bdc627ba99d23
Sahil sharma <sahilsharma2000dtc@gmail.com>
2026-08-06 01:03:10 +0530

    category repository functions implemented

--- apps/backend/internal/repository/repositories.go
+++ apps/backend/internal/repository/repositories.go
@@ -3,13 +3,15 @@ package repository
 import "github.com/SAHIL-Sharma21/go-taskForge/internal/server"
 
 type Repositories struct {
-	Todo    *TodoRepository
-	comment *CommentRepository
+	Todo     *TodoRepository
+	Comment  *CommentRepository
+	Category *CategoryRepository
 }
 
 func NewRepositories(s *server.Server) *Repositories {
 	return &Repositories{
-		Todo:    NewTodoRepository(s),
-		comment: NewCommentRepositiory(s),
+		Todo:     NewTodoRepository(s),
+		Comment:  NewCommentRepositiory(s),
+		Category: NewCategoryRepository(s),
 	}
 }
